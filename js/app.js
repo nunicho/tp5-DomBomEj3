ca0d398c2987b509a651c2be56b689d277a02044
Mauricio-Alonso <conta.alonso@gmail.com>
2022-08-08 13:23:47 -0300

    Terminé ejercicio 3.

--- js/app.js
+++ js/app.js
@@ -6,10 +6,41 @@ para ese fin.
 
 */
 
-let tareas = []
-
-function agregar (){
-let tarea 
+let tareas = ['comer', 'beber', 'cantar']
 
 
+function sumarTarea(){
+        let nuevaTarea = prompt("Detalle la tarea a ingresar")
+        tareas.push(nuevaTarea)
 }
+
+
+function eliminarTarea(){
+        let eliminarTarea=prompt("Ingrese tarea a eliminar")
+        let tareasFiltradas = tareas.filter((tareas) => {
+        return tareas != eliminarTarea;
+        });
+        tareas = tareasFiltradas;
+}
+
+function mostrarTarea(){
+        let ul = document.createElement('ul');  
+        document.getElementById('lugarLista').appendChild(ul);
+        tareas.forEach(itemTareas);
+        function itemTareas(element, posicion) {
+            let li = document.createElement('li');
+            ul.appendChild(li);
+            li.innerHTML=li.innerHTML + element +` es la tarea `+ posicion
+        }
+    };
+
+    
+
+
+
+
+/*
+investigué en 
+https://stackoverflow.com/questions/11351135/create-ul-and-li-elements-in-javascript
+
+*/
